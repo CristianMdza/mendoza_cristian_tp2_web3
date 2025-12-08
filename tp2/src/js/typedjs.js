318d0f49fbb1cc51bba2a74707ca5b4377d5bf34
CristianMdza <kristiankno17@gmail.com>
2025-12-07 21:36:03 -0500

    changement 15 (Ajout de Titre + Texte de données de mtl par la méthode d'animation Typedjs dans l'interface de la visère futuriste, code html, code css et code js pour cela)

--- tp2/src/js/typedjs.js
+++ tp2/src/js/typedjs.js
@@ -1,1 +1,64 @@
+// CODE TYPEDJS POUR L'AFFICHAGE DU TEXTE ANIMÉ DANS LE HUD
 
+import Typed from "typed.js";
+
+const titleText = "Montréal, Québec";
+
+const dataText = 
+`Température: -12°C
+Langues: Français / Anglais
+Population: 2.2M
+Signaux urbains: Actifs
+Reconnaissance faciale: Activée`;
+
+let typedTitle = null;
+let typedData = null;
+
+/* FONCTION "LOOP" DE DÉMARRAGE DE LA SÉQUENCE TYPEDJS */
+
+function startSequence() {
+
+  // DÉTRUIT LES INSTANCES PRÉCÉDENTES
+  if (typedTitle) typedTitle.destroy();
+  if (typedData) typedData.destroy();
+
+  // RESET DU HTML, VIDE LE PLACEHOLDER
+  document.querySelector("#typed-title").textContent = "";
+  document.querySelector("#typed-info").textContent = "";
+
+  /* TITRE PRINCIPAL */
+  typedTitle = new Typed("#typed-title", {
+
+    strings: [titleText],
+    typeSpeed: 60,
+    showCursor: false,
+    loop: false,
+
+    onComplete: () => {
+
+      /* DONNÉES INFO */
+      typedData = new Typed("#typed-info", {
+
+        strings: [dataText],
+        typeSpeed: 28,
+
+        backSpeed: 20,
+        backDelay: 2000,
+
+        showCursor: true,
+        cursorChar: "▌",
+        loop: false,
+
+        onComplete: () => {
+          setTimeout(startSequence, 1500);
+        }
+
+      });
+
+    }
+
+  });
+}
+
+/* DÉMARRAGE DE LA SÉQUENCE */
+startSequence();
